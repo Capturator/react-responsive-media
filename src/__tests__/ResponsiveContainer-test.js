c67d249f32ba1828fd2b43ad734cd35df7da122b
Marco Loche <marco.loche@capturator.com>
2015-04-02 13:41:10 +0200

    improving test

--- src/__tests__/ResponsiveContainer-test.js
+++ src/__tests__/ResponsiveContainer-test.js
@@ -1,16 +1,44 @@
 // __tests__/main-test.js
+jest.dontMock('../ResponsiveContainer');
+jest.dontMock('../lib/breakpoints');
+
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
 
 describe('ResponsiveContainer', function () {
-    var container;
+    var ResponsiveContainerComponent;
     beforeEach(function () {
-        container = require('../ResponsiveContainer');
+        ResponsiveContainerComponent = require('../ResponsiveContainer');
     });
 
     afterEach(function () {
-        container = null;
+        ResponsiveContainerComponent = null;
     });
 
     it('should be an object', function () {
-        expect(container).toBeDefined();
+        expect(ResponsiveContainerComponent).toBeDefined();
     });
+
+    describe(' when rendered as an application Container', function () {
+        var container;
+        var originalWindowMatchMedia;
+        beforeEach(function () {
+            originalWindowMatchMedia =  window.matchMedia;
+            window.matchMedia = jest.genMockFunction().mockImplementation(function() {
+                return { addListener: function(){}};
+            });
+            var mediaQueries = {'small': 200};
+            container = TestUtils.renderIntoDocument(<ResponsiveContainerComponent bp={mediaQueries}><div>{'foo'}</div></ResponsiveContainerComponent>);
+        });
+
+        afterEach(function () {
+            container = null;
+            window.matchMedia = originalWindowMatchMedia;
+        });
+
+        it('should be a composite component', function () {
+            expect(TestUtils.isCompositeComponent(container)).toBe(true);
+        });
+    });
+
 });
